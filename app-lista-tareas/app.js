61cb96871604e6dbf175324fb239aa8978975d80
grovatti <guillermo.rovatti@gmail.com>
2022-02-04 19:53:49 -0300

    feat: tarea02

--- app-lista-tareas/app.js
+++ app-lista-tareas/app.js
@@ -1,11 +1,16 @@
 require('colors');
-const { mostrarMenu } = require('../helpers/mensajes');
+const { mostrarMenu, pausa } = require('../helpers/mensajes');
 
 console.clear();
 
 const main = async() => {
     console.log('Hola Mundo'.rainbow);
-    mostrarMenu();
+    let opcion = 1;
+    do {
+        opcion = await mostrarMenu(opcion - 1);
+        await pausa();
+    } while (opcion !== '0');
+    console.clear();
 }
 
 main();
